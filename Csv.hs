6e548b3e7287f69e8fdfda27c2e999ede45734c9
Katsutoshi Itoh <cusea110@gmail.com>
2012-12-14 10:40:42 +0900

    Like a service

--- Csv.hs
+++ Csv.hs
@@ -1,28 +1,26 @@
 {-# LANGUAGE QuasiQuotes, TemplateHaskell, OverloadedStrings, TypeFamilies, MultiParamTypeClasses #-}
-{-# LANGUAGE FlexibleInstances, GADTs #-}
+{-# LANGUAGE FlexibleInstances #-}
 module Csv where
 
 import Yesod
-import Control.Applicative ((<$>))
+import Control.Applicative ((<$>),(<*>))
 import Control.Arrow ((***))
+import Data.List (transpose)
 import Data.Text (Text)
 import qualified Data.Text as T
+import System.Random (getStdGen, randomRs)
 
 data C = C
 
 mkYesod "C" [parseRoutes|
-/ RootR GET
-/download/#Text DownloadR GET
+/ RootR GET POST
 |]
 
 instance Yesod C
 instance RenderMessage C FormMessage where
   renderMessage _ _ = defaultFormMessage
 
-newtype CSV a where
-  CSV :: ([Text],[[a]]) -> CSV a
-unCsv :: CSV a -> ([Text],[[a]])
-unCsv (CSV x) = x
+newtype CSV a = CSV { unCsv :: ([Text],[[a]]) }
 instance Show a => ToContent (CSV a) where
   toContent = toContent.trans
     where
@@ -35,35 +33,75 @@ instance Show a => ToContent (CSV a) where
       toText :: Show a => a -> Text
       toText = T.pack . show
 
-newtype RepCsv a where
-  RepCsv :: (CSV a) -> RepCsv a
+newtype RepCsv a = RepCsv (CSV a)
 instance Show a => HasReps (RepCsv a) where
   chooseRep (RepCsv c) _ = return (typeOctet, toContent c)
 
-data F = F {name::Text}
+data F = F {name::Text, candidates :: Textarea, num :: Int}
+       deriving Show
 type Form x = Html -> MForm C C (FormResult x, Widget)
-sampleForm :: Form F
-sampleForm = renderDivs $ F <$> areq textField "file name" Nothing
+sampleForm :: Maybe F -> Form F
+sampleForm mf = renderDivs $ F 
+             <$> areq textField "file name" (name <$> mf)
+             <*> areq textareaField "candidates" (candidates <$> mf)
+             <*> areq intField "generate number" (num <$> mf)
 
 getRootR :: Handler RepHtml
 getRootR = do
-  ((r, w), e) <- runFormGet sampleForm
-  case r of
-    FormSuccess x -> redirect (DownloadR $ name x)
-    _ -> defaultLayout [whamlet|
-<form action=@{RootR} enctype=#{e}>
+  (w, e) <- generateFormPost $ sampleForm $ Just $ F "sample" (Textarea "type:X,Y,Z\ncost:100,200,300,400,500,600,700\ncategory:Kid,Man,Women") 1000
+  defaultLayout [whamlet|
+<form method=post action=@{RootR} enctype=#{e}>
   ^{w}
   <input type=submit>
 |]
 
-getDownloadR :: Text -> Handler (RepCsv Int)
-getDownloadR fn = do
-  download (fn `T.append` ".csv") $ CSV (["a","b","c"], [[1,2,3],[4,5,6],[7,8,9]])
+postRootR :: Handler (RepCsv Text)
+postRootR = do
+  ((r, _), _) <- runFormPost $ sampleForm Nothing
+  case r of
+    FormSuccess x -> download x
+    _ -> redirect RootR
 
-download :: Yesod m => Text -> CSV Int -> GHandler m s (RepCsv Int)
-download fn rep = do
+download :: F -> Handler (RepCsv Text)
+download (F fn cs n) = do
+  rows <- liftIO $ generateFrom (unTextarea cs) n
+  downloadCSV (fn `T.append` ".csv") $ CSV rows
+
+downloadCSV :: Yesod m => Text -> CSV Text -> GHandler m s (RepCsv Text)
+downloadCSV fn rep = do
   setHeader "Content-Disposition" $ T.append "attachiment; filename=" fn
   return (RepCsv rep)
+
+generateFrom :: Text -> Int -> IO ([Text], [[Text]])
+generateFrom cs n = fmap toCSV $ mapM (genR n) $ extract cs
+
+extract :: Text -> [(Text, [Text])]
+extract = map (toDataList . T.break (==':')) . lines'
+  where
+    toDataList = id *** (T.split (==',') . T.tail)
+
+lines' :: Text -> [Text]
+lines' ps | T.null ps = []
+          | otherwise = h : case T.uncons t of
+  Nothing -> []
+  Just (c,t')
+    | c == '\n' -> lines' t'
+    | c == '\r' -> case T.uncons t' of
+      Just ('\n',t'') -> lines' t''
+      _               -> lines' t'
+  where
+    (h,t) = T.span notEOL ps
+    notEOL c = c /= '\n' && c /= '\r'
+
+genR :: Int -> (Text, [Text]) -> IO (Text, [Text])
+genR n (h, cs) = do
+  g <- getStdGen
+  return $ (h, take n $ map (cs!!) $ randomRs (0, length cs-1) g)
+
+toCSV :: [(Text, [Text])] -> ([Text], [[Text]])
+toCSV = (id *** transpose) . foldr f ([], [])
+  where
+    f (h, b) (hs, bs) = (h:hs, b:bs)
 
 main :: IO ()
 main = warpDebug 3000 C
